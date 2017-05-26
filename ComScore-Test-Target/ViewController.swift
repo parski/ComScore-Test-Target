e6147da543a1b2f9eda9f41f5d915b5d43f18288
Pär Strindevall <par@strindevall.com>
2017-05-26 13:58:33 +0200

    Add problem demonstration.

--- ComScore-Test-Target/ViewController.swift
+++ ComScore-Test-Target/ViewController.swift
@@ -7,19 +7,12 @@
 //
 
 import UIKit
+import ComScore
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
+    // Able to access and use ComScore framework
+    let scorAnalyticsClass = SCORAnalytics.self
 
 }
 
--- ComScore-Test-TargetTests/ComScore_Test_TargetTests.swift
+++ ComScore-Test-TargetTests/ComScore_Test_TargetTests.swift
@@ -7,30 +7,62 @@
 //
 
 import XCTest
+
 @testable import ComScore_Test_Target
 
 class ComScore_Test_TargetTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_firstExamble() {
+        
+        /* Problem illustration:
+         
+         Line 11:
+         @testable import ComScore_Test_Target
+         
+         Should give this target access to frameworks linked to the test target.
+         
+         Error: Use of unresolved identifier 'SCORAnalytics'
+         
+         Try it yourself by comparing Shift + Command + R (build main target) and Shift + Command + U (build test target).
+         
+         The main target builds just fine but the test target gets a compilation error.
+         
+        */
+        _ = SCORAnalytics()
     }
+
+}
+
+//import ComScore
+
+class ComScore_Test_TargetTestsSecondExample: XCTestCase {
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func test_secondExample() {
+        
+        /* Problem illustration:
+         
+         Uncomment line 36, 51-54, 63-65.
+         
+         This import a separate copy of the ComScore framework to the test target.
+         This means that asserting equality between two references to a ComScore class within the same target is completely fine:
+         
+         */
+        
+//        let testTargetScorAnalyticsClass = SCORAnalytics.self
+//        let anotherTestTargetScorAnalyticsClass = SCORAnalytics.self
+//        
+//        XCTAssertTrue(testTargetScorAnalyticsClass == anotherTestTargetScorAnalyticsClass)
+        
+        /*
+         
+         But asserting equality of the same class in another target fails. 
+         This is because each target gets a different set of symbols when each imports the framework and the classes are in fact not equal.
+         
+         */
+        
+//        let mainTargetScorAnalyticsClass = ViewController(nibName: nil, bundle: nil).scorAnalyticsClass
+//        
+//        XCTAssertTrue(testTargetScorAnalyticsClass == mainTargetScorAnalyticsClass)
     }
     
 }
